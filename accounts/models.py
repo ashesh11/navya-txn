193deb7d03d26c5a60deeb9424db658e1949921b
asheshbh11 <136300236+asheshbh11@users.noreply.github.com>
2024-10-30 10:22:57 +0545

    feat(account): custom user model with its migration

---
+++ accounts/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, AbstractUser
+
+class UserAccountManager(BaseUserManager):
+    def create_user(self, email, name, password=None, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_manager", False)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(email, name, password, **extra_fields)
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_manager = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+
+    def __str__(self):
+        return self.email
