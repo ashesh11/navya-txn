6e5d9177380eea558b3d532f56e862b65aee8e79
asheshbh11 <136300236+asheshbh11@users.noreply.github.com>
2024-11-01 20:09:30 +0545

    feat(txn): endpoint to pdf download all txns or a detail

--- transaction/urls.py
+++ transaction/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from transaction.views import TransactionListView, TransactionDetailView
+from transaction.views import TransactionListView, TransactionDetailView, AllTransactionPDFView, DetailTransactionPDFView
 
 urlpatterns = [
     path('transactions/', TransactionListView.as_view(), name='list-txn'),
-    path('transactions/<str:txn_id>/', TransactionDetailView.as_view(), name='detail-txn')
+    path('transactions/<str:txn_id>/', TransactionDetailView.as_view(), name='detail-txn'),
+    path('pdf/transactions/', AllTransactionPDFView.as_view(), name='all-txns-pdf'),
+    path('pdf/transactions/<str:txn_id>/', DetailTransactionPDFView.as_view(), name='one-txn-pdf'),
 ]
