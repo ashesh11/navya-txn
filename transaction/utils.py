f6cacc89451fae7debf8c8503fb823e70983d8b3
Ashesh Bhattarai <ashesh.bh@gmail.com>
2024-11-02 13:51:21 +0545

    refac/reponse (#5)
    * refac: updated documentation along with responses
    
    * docs: added readme and added psql
    
    ---------
    
    Co-authored-by: asheshbh11 <136300236+asheshbh11@users.noreply.github.com>

--- transaction/utils.py
+++ transaction/utils.py
@@ -4,27 +4,30 @@ from django.template import loader
 from xhtml2pdf import pisa
 
 def generate_transaction_pdf(transaction, type):
-    if type == 'detail':
-        template = loader.get_template('transaction/detail_txn_template.html')
-        context_name = 'transaction'
-    elif type == 'all':
-        template = loader.get_template('transaction/all_txns_template.html')
-        context_name = 'transactions'
+    try:
+        if type == 'detail':
+            template = loader.get_template('transaction/detail_txn_template.html')
+            context_name = 'transaction'
+        elif type == 'all':
+            template = loader.get_template('transaction/all_txns_template.html')
+            context_name = 'transactions'
 
-    context = {
-        context_name: transaction,
-        'logo_path': os.path.abspath('logo.jpeg') 
-    }
+        context = {
+            context_name: transaction,
+            'logo_path': os.path.abspath('logo.jpeg') 
+        }
 
-    # Render the template with context
-    html = template.render(context)
+        # Render the template with context
+        html = template.render(context)
 
-    buffer = BytesIO()
+        buffer = BytesIO()
 
-    # Convert HTML to PDF and store it in the buffer
-    pisa.CreatePDF(html, dest=buffer)
+        # Convert HTML to PDF and store it in the buffer
+        pisa.CreatePDF(html, dest=buffer)
 
-    # Reset buffer position to the beginning for reading
-    buffer.seek(0)
+        # Reset buffer position to the beginning for reading
+        buffer.seek(0)
 
-    return buffer
+        return buffer
+    except Exception:
+        return None
--- transaction/views.py
+++ transaction/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from transaction.services import TransactionServices
 from transaction.serializers import TransactionSerializer
 from transaction.utils import generate_transaction_pdf
-from account.authentication import AccountJWTAuthentication
 from account.permissions import TransactionListViewPermissionHandler, TransactionDetailViewPermissionHandler
 
 
@@ -13,16 +12,22 @@ class TransactionListView(APIView):
     service = TransactionServices()
 
     def get(self, request):
-        transactions = self.service.list()
-        serializer = TransactionSerializer(transactions, many=True)
-        return Response(data={'data': serializer.data}, status=200)
+        try:
+            transactions = self.service.list()
+            serializer = TransactionSerializer(transactions, many=True)
+            return Response(data={'data': serializer.data}, status=200)
+        except Exception as e:
+            return Response({'error': e}, status=400)
 
     def post(self, request):
-        serializer = TransactionSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        transaction = self.service.create(serializer.validated_data)
-        data = {'txn_id': transaction.txn_id}
-        return Response(data={'data': data}, status=200)
+        try:
+            serializer = TransactionSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            transaction = self.service.create(serializer.validated_data)
+            data = {'txn_id': transaction.txn_id}
+            return Response(data={'data': data}, status=200)
+        except Exception as e:
+            return Response({'error': e}, status=400)
 
 
 class TransactionDetailView(APIView):
@@ -30,54 +35,74 @@ class TransactionDetailView(APIView):
     permission_classes = [TransactionDetailViewPermissionHandler]
 
     def get(self, request, txn_id):
-        txn = self.service.retrieve(txn_id=txn_id)
-        if not txn:
-            return Response('No content', status=204)
-        serializer = TransactionSerializer(txn)
-        return Response(data={'data': serializer.data}, status=200)
+        try:
+            txn = self.service.retrieve(txn_id=txn_id)
+            if not txn:
+                return Response('No content', status=204)
+            serializer = TransactionSerializer(txn)
+            return Response(data={'data': serializer.data}, status=200)
+        except Exception as e:
+            return Response({'error': e}, status=400)
 
     def put(self, request, txn_id):
-        serializer = TransactionSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.service.update(txn_id=txn_id, data=serializer.validated_data)
-        updated_txn = self.service.retrieve(txn_id=txn_id)
-        serializer = TransactionSerializer(updated_txn)
-        return Response(data={'data': serializer.data}, status=200)
+        try:
+            serializer = TransactionSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.service.update(txn_id=txn_id, data=serializer.validated_data)
+            updated_txn = self.service.retrieve(txn_id=txn_id)
+            serializer = TransactionSerializer(updated_txn)
+            return Response(data={'data': serializer.data}, status=200)
+        except Exception as e:
+            return Response({'error': e}, status=400)
 
     def patch(self, request, txn_id):
-        serializer = TransactionSerializer(data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        self.service.update(txn_id=txn_id, data=serializer.validated_data)
-        updated_txn = self.service.retrieve(txn_id=txn_id)
-        serializer = TransactionSerializer(updated_txn)
-        return Response(data={'data': serializer.data}, status=200)
+        try:
+            serializer = TransactionSerializer(data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            self.service.update(txn_id=txn_id, data=serializer.validated_data)
+            updated_txn = self.service.retrieve(txn_id=txn_id)
+            serializer = TransactionSerializer(updated_txn)
+            return Response(data={'data': serializer.data}, status=200)
+        except Exception as e:
+            return Response({'error': e}, status=400)
 
     def delete(self, request, txn_id):
-        self.service.delete(txn_id=txn_id)
-        return Response(status=200)
+        try:
+            self.service.delete(txn_id=txn_id)
+            return Response(status=200)
+        except Exception as e:
+            return Response({'error': e}, status=400)
     
 
 class AllTransactionPDFView(APIView):
     service = TransactionServices()
 
     def get(self, request):
-        txns = self.service.queryset.filter(approve_status='approved')
-        if not txns:
-            return Response('No available/approved transactions to create PDF', status=204)
-        pdf = generate_transaction_pdf(txns, type='all')
-        response = HttpResponse(pdf, content_type='application/pdf')
-        response['Content-Disposition'] = 'attachment; filename="transactions.pdf"'
-        return response
+        try:
+            txns = self.service.queryset.filter(approve_status='approved')
+            if not txns:
+                return Response({'error': 'No available/approved transactions to create PDF'}, status=204)
+            pdf = generate_transaction_pdf(txns, type='all')
+            response = HttpResponse(pdf, content_type='application/pdf')
+            response['Content-Disposition'] = 'attachment; filename="transactions.pdf"'
+            return response
+        except Exception as e:
+            return Response({'error': e}, status=400)
 
 
 class DetailTransactionPDFView(APIView):
     service = TransactionServices()
 
     def get(self, request, txn_id):
-        txn = self.service.queryset.filter(txn_id=txn_id, approve_status='approved').first()
-        if not txn:
-            return Response('Transaction not available or rejected', status=204)
-        pdf = generate_transaction_pdf(txn, type='detail')
-        response = HttpResponse(pdf, content_type='application/pdf')
-        response['Content-Disposition'] = 'attachment; filename="transactions.pdf"'
-        return response
+        try:
+            txn = self.service.queryset.filter(txn_id=txn_id, approve_status='approved').first()
+            if not txn:
+                return Response({'error': 'Transaction not available or rejected'}, status=204)
+            pdf = generate_transaction_pdf(txn, type='detail')
+            if not pdf:
+                return Response({'error': 'Unable to create PDF'}, status=400)
+            response = HttpResponse(pdf, content_type='application/pdf')
+            response['Content-Disposition'] = 'attachment; filename="transactions.pdf"'
+            return response
+        except Exception as e:
+            return Response({'error': e}, status=400)
